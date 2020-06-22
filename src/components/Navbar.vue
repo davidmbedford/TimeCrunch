49768912fbd6522b5ed268157e4e3869abf9ac07
davidmbedford <david.m.bedford1@gmail.com>
2020-06-22 13:24:53 -0400

    navbar functionally is troubleshot - switched to app-bar from toolbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <nav>
-    <v-toolbar flat app color="grey lighten-4">
+    <v-app-bar flat app color="grey lighten-4">
+      <!-- <v-icon left class="grey--text" @click="drawer = !drawer">mdi-home</v-icon> -->
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text--uppercase grey--text">
         <span class="font-weight-light">Time</span>
         <span>Crunch</span>
@@ -10,12 +12,22 @@
         <span>Sign Out</span>
         <v-icon right>mdi-login</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" color="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
+
   </nav>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      drawer: false
+    }
+  }
   
 }
 </script>
