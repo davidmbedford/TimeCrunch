a33b7eb0182c607c8a61ead9cbbca82c5ef7a2d5
davidmbedford <david.m.bedford1@gmail.com>
2020-06-23 13:31:43 -0400

    Navbar expanded to have secondary routing - projects filtering complete

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,35 @@
         <span>Crunch</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn depressed color="grey">
+
+    <template>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn 
+            depressed
+            color="grey" 
+            v-bind="attrs"
+            v-on="on"
+            class="mx-1"
+          >
+            <v-icon left>mdi-arrow-down</v-icon>
+            <span>dropdown</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+            >
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+
+    <v-btn depressed color="grey" class="mx-1">
         <span>Sign Out</span>
         <v-icon right>mdi-login</v-icon>
       </v-btn>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,45 @@
 <template>
   <div class="projects">
     <h1>My Projects</h1>
+
     <v-container class="my-5">
-      <p>Lorem dolor sit consectetur adipisicing elit. Corrupti dolore optio, saepe distinctio tenetur fugit commodi molestiae nemo quos excepturi, mollitia consectetur laudantium velit cum voluptatibus natus sed!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolore optio, saepe distinctio itaque tenetur fugit commodi molestiae nemo quos excepturi, mollitia consectetur repellendus laudantium velit cum voluptatibus natus sed?</p>
+      <v-expansion-panels>
+        <v-expansion-panel v-for="project in myProjects" :key="project.title">
+          <v-expansion-panel-header>{{ project.title }}</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card>
+              <v-card-text class="px-4 py-0 grey--text">
+                <div class="font-weight-bold">due by {{ project.due }}</div>
+                <div>{{project.content}}</div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-container>
+
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a commune for Ummhs', person: 'The Net Ninja', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'}
+      ]
+    }
+  },
+  computed: {
+    myProjects(){
+      return this.projects.filter(project => {
+        return project.person === 'The Net Ninja'
+      })
+    }
+  }
+}
+</script>
