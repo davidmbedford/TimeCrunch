883667a3d7d8f1c90f9d1cc5e67a644e9b1a7f03
davidmbedford <david.m.bedford1@gmail.com>
2020-06-23 13:54:37 -0400

    project button added with dialog

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -49,6 +49,9 @@
           </v-avatar>
           <p class="white--text subheading mt-1">The Net Ninja</p>
         </v-col>
+        <v-col>
+          <Popup />
+        </v-col>
       </v-row>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -66,7 +69,10 @@
 </template>
 
 <script>
+import Popup from './Popup'
+
 export default {
+  components: { Popup },
   data() {
     return {
       drawer: false,
---
+++ src/components/Popup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-center">
+  <v-dialog v-model="dialog" max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        class="success"
+        text 
+        v-bind="attrs"
+        v-on="on"
+      >
+        Add a new project
+      </v-btn>
+    </template>
+      <v-card>
+        <v-card-title>
+          <h2>Add a new project</h2>
+        </v-card-title>
+      </v-card>
+  </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
